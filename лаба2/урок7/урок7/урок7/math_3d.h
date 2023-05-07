9751f9b066825a6db10058e65ca7861fc7e1f240
PirateThunder <ilyakhakimov03@gmail.com>
2023-05-07 21:31:38 +0500

    m

---
+++ лаба2/урок7/урок7/урок7/math_3d.h
@@ -0,0 +1,64 @@
+/*
+
+    Copyright 2010 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MATH_3D_H
+#define	MATH_3D_H
+
+
+struct Vector3f
+{
+    float x;
+    float y;
+    float z;
+
+    Vector3f()
+    {
+    }
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
+
+
+struct Matrix4f
+{
+    float m[4][4];
+
+    inline Matrix4f operator*(const Matrix4f& Right) const
+    {
+        Matrix4f Ret;
+
+        for (unsigned int i = 0; i < 4; i++) {
+            for (unsigned int j = 0; j < 4; j++) {
+                Ret.m[i][j] = m[i][0] * Right.m[0][j] +
+                    m[i][1] * Right.m[1][j] +
+                    m[i][2] * Right.m[2][j] +
+                    m[i][3] * Right.m[3][j];
+            }
+        }
+
+        return Ret;
+    }
+};
+
+
+#endif	/* MATH_3D_H */
